da2ea3da896fe4323a3d3cb5833b15e09c4c384c
haniful-islam <haniful360@gmail.com>
2022-05-12 21:36:19 +0600

    service responsive

--- src/Pages/Home/Home/Service/Service.js
+++ src/Pages/Home/Home/Service/Service.js
@@ -1,14 +1,15 @@
 import React from 'react';
-
+import './Service.css';
 const Service = ({service}) => {
     const {id, name , price, description, img} = service;
+    
     return (
-        <div>
-            <img width="400" height="350"src={img} alt=""/>
-            <h2>{name}</h2>
-            <p>{price}</p>
+        <div className="service">
+            <img width="360" height="350"src={img} alt=""/>
+            <h2 className="my-2">{name}</h2>
+            <p className="text-info">Price:{price}</p>
             <p><small>{description}</small></p>
-            <button className="btn btn-info text-white">Book:{name}</button>
+            <button  className="btn btn-info text-white">Book:{name}</button>
         </div>
     );
 };
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -3,23 +3,23 @@ import Service from '../Home/Service/Service';
 import './Services.css';
 
 const Services = () => {
-    const [services, setServices]  = useState([]);
+    const [services, setServices] = useState([]);
 
-    useEffect(() =>{
+    useEffect(() => {
         fetch('services.json')
-        .then(response => response.json())
-        .then(data => setServices(data))
-    },[])
+            .then(response => response.json())
+            .then(data => setServices(data))
+    }, [])
     return (
         <div>
             <h1 className="text-center text-info my-4">Our Services</h1>
             <div className="services-container">
-            {
-            services.map(service =><Service
-            key = {service.id}
-            service = {service}
-            ></Service>)
-            }
+                {
+                    services.map(service => <Service
+                        key={service.id}
+                        service={service}
+                    ></Service>)
+                }
             </div>
         </div>
     );
